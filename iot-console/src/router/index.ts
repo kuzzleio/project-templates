9d57908d1baf880621dcda138d2dba38513aa52a
Aschen <adrien@maret.email>
2022-07-29 10:09:04 +0200

    add dashboard wip

--- iot-console/src/router/index.ts
+++ iot-console/src/router/index.ts
@@ -11,6 +11,15 @@ import {
   KPageNotFound,
 } from "@kuzzleio/kuzzle-application-builder";
 import { Store } from "vuex";
+import {
+  createDashboardView,
+  MapWidget,
+  MapWidgetForm,
+  ChartWidget,
+  ChartWidgetForm,
+  TableWidget,
+  TableWidgetForm,
+} from '@kuzzleio/dashboard-builder'
 
 import { kuzzle } from "../services/kuzzle";
 import Login from "../views/Login.vue";
@@ -19,8 +28,77 @@ export const createRouter = (
   store: Store<RootState>,
   appDefinition: AppChunk[] = []
 ): VueRouter => {
+
+  appDefinition.push({
+    name: 'dashboards',
+    label: 'Dashboards',
+    icon: (faTachometerAlt),
+    vuejsRoute: {
+      path: '/dashboards',
+      name: 'dashboards',
+      component: DashboardWrapper,
+      meta: {
+        breadcrumb: 'locales.nav.dashboards'
+      },
+      children: [
+        {
+          path: 'v/:dashboardId',
+          name: 'dashboard-view',
+          component: Dashboard,
+          meta: {
+            breadcrumb: 'locales.dashboards.nav.view'
+          },
+          props: (route: Route): { id: string } => ({
+            id: route.params.dashboardId
+          })
+        },
+        {
+          path: 'new',
+          name: 'create-dashboard',
+          component: Dashboard,
+          meta: {
+            breadcrumb: 'locales.dashboards.nav.create'
+          }
+        },
+        {
+          path: '',
+          name: 'dashboards',
+          component: DashboardList,
+          meta: {
+            breadcrumb: 'locales.dashboards.nav.list'
+          }
+        },
+      ]
+    },
+  });
+
   const appRoutes = generateRoutes(appDefinition);
   const sidebarItems = generateMenuItems(appDefinition);
+  const kdbWidgets = [
+    {
+      name: 'map',
+      label: 'Map',
+      component: MapWidget,
+      formComponent: MapWidgetForm,
+      icon: 'map'
+    },
+    {
+      name: 'chart',
+      label: 'Chart',
+      component: ChartWidget,
+      formComponent: ChartWidgetForm,
+      icon: 'chart-bar'
+    },
+    {
+      name: 'table',
+      label: 'Table',
+      component: TableWidget,
+      formComponent: TableWidgetForm,
+      icon: 'table'
+    },
+    // You can add other custom widgets here...
+  ];
+  const DashboardView = createDashboardView({ customWidgets: kdbWidgets })
 
   const router = new VueRouter({
     base: process.env.BASE_URL,
