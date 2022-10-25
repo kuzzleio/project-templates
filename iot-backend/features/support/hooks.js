ea596f32345a98086c345974c127aea01af8900e
Kuruyia <8174691+Kuruyia@users.noreply.github.com>
2022-10-25 08:35:42 +0200

    Add Kuzzle ESLint plugin (#6)

--- iot-backend/features/support/hooks.js
+++ iot-backend/features/support/hooks.js
@@ -1,31 +1,36 @@
-'use strict';
+"use strict";
 
-const _ = require('lodash');
-const { After, Before, BeforeAll } = require('cucumber');
-const { Kuzzle, WebSocket, Http } = require('kuzzle-sdk');
+const _ = require("lodash");
+const { After, Before, BeforeAll } = require("cucumber");
+const { Kuzzle, WebSocket, Http } = require("kuzzle-sdk");
 
-const testMappings = require('../fixtures/mappings');
-const testSecurities = require('../fixtures/securities');
-const World = require('./world');
+const testMappings = require("../fixtures/mappings");
+const testSecurities = require("../fixtures/securities");
+const World = require("./world");
 
 async function truncateCollection(sdk, index, collection, query = {}) {
   await sdk.collection.refresh(index, collection);
-  await sdk.document.deleteByQuery(index, collection, { query }, { lang: 'koncorde' });
+  await sdk.document.deleteByQuery(
+    index,
+    collection,
+    { query },
+    { lang: "koncorde" }
+  );
 }
 
 async function resetTenant(sdk, group, name) {
   await sdk
     .query({
-      controller: 'multi-tenancy/tenant',
-      action: 'delete',
+      controller: "multi-tenancy/tenant",
+      action: "delete",
       group,
       name,
     })
     .catch(() => {});
 
   await sdk.query({
-    controller: 'multi-tenancy/tenant',
-    action: 'create',
+    controller: "multi-tenancy/tenant",
+    action: "create",
     group,
     name,
   });
@@ -34,7 +39,7 @@ async function resetTenant(sdk, group, name) {
 BeforeAll({ timeout: -1 }, async function () {
   const world = new World({});
 
-  if (world.port.toString() === '443') {
+  if (world.port.toString() === "443") {
     throw new Error(
       `Detecting tests on port 443. Are you targeting a production server? (${world.host})`
     );
@@ -46,26 +51,24 @@ BeforeAll({ timeout: -1 }, async function () {
 
   await world.sdk.connect();
 
-  console.log('Loading default securities..');
+  console.log("Loading default securities..");
 
   await world.sdk.query({
-    controller: 'admin',
-    action: 'loadSecurities',
+    controller: "admin",
+    action: "loadSecurities",
     body: testSecurities,
-    refresh: 'wait_for',
-    onExistingUsers: 'overwrite',
+    refresh: "wait_for",
+    onExistingUsers: "overwrite",
   });
 
   await world.sdk.query({
-    controller: 'admin',
-    action: 'loadMappings',
+    controller: "admin",
+    action: "loadMappings",
     body: testMappings,
-    refresh: 'wait_for',
+    refresh: "wait_for",
   });
 
-  await Promise.all([
-
-  ]);
+  await Promise.all([]);
 });
 
 Before({ timeout: -1 }, async function () {
@@ -75,30 +78,29 @@ Before({ timeout: -1 }, async function () {
   this.sdk = new Kuzzle(new WebSocket(this.host, { port: this.port }));
 
   await this.sdk.connect();
-  await this.sdk.auth.login('local', {
-    username: 'test-admin',
-    password: 'password',
+  await this.sdk.auth.login("local", {
+    username: "test-admin",
+    password: "password",
   });
 
-  await Promise.all([
-  ]);
+  await Promise.all([]);
 });
 
 After(async function () {
   // Clean values stored by the scenario
   this.props = {};
 
-  if (this.sdk && typeof this.sdk.disconnect === 'function') {
+  if (this.sdk && typeof this.sdk.disconnect === "function") {
     this.sdk.disconnect();
   }
 });
 
 // realtime hooks ==============================================================
 
-After({ tags: '@realtime' }, function () {
+After({ tags: "@realtime" }, function () {
   if (_.isEmpty(this.props.subscriptions)) {
     throw new Error(
-      '@realtime time has been set but no subscriptions have been made.'
+      "@realtime time has been set but no subscriptions have been made."
     );
   }
 
--- iot-backend/lib/application-builder/commons.ts
+++ iot-backend/lib/application-builder/commons.ts
@@ -1,12 +1,12 @@
-import { ApplicationBuilder, DeviceManagerPlugin } from '@kuzzleio/iot-backend';
+import { ApplicationBuilder, DeviceManagerPlugin } from "@kuzzleio/iot-backend";
 
-import { ExampleApplication } from '../ExampleApplication';
-import { ExampleDecoder } from '../modules/decoders';
+import { ExampleApplication } from "../ExampleApplication";
+import { ExampleDecoder } from "../modules/decoders";
 
 export function registerCommons(app: ExampleApplication) {
-  const deviceManager = app.plugin.get('device-manager') as DeviceManagerPlugin;
+  const deviceManager = app.plugin.get("device-manager") as DeviceManagerPlugin;
 
-  ApplicationBuilder.commons(commons => {
+  ApplicationBuilder.commons((commons) => {
     commons.decoder.register(new ExampleDecoder(deviceManager.measures));
   });
 }
